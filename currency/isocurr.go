64d3bd2eba2c4fc8fe1d47f86789cd8f55cb103a
Adrian Lezcano <adrianhlezcano@gmail.com>
2022-08-23 12:54:38 -0300

    Initial commit

---
+++ currency/isocurr.go
@@ -0,0 +1,26 @@
+package isocurr
+
+import "fmt"
+
+type Currency struct {
+    entity string
+    name   string
+    code   string
+    minor_unit int
+}
+
+var argCurr = &Currency{"ARGENTINA", "Argentine Peso", "ARS", 2}
+var brlCurr = &Currency{"BRAZIL", "Brazilian Real", "BRL", 2}
+var ilsCurr = &Currency{"ISRAEL", "New Israeli Sheqel", "ILS", 2}
+var usdCurr = &Currency{"UNITED STATES", "US Dollar", "USD", 2}
+var currencyList = map[string]*Currency{"ARGENTINA": argCurr, "BRAZIL": brlCurr, "ISRAEL": ilsCurr, "UNITED STATES": usdCurr}
+
+func GetCurrency(countryName string) (curr Currency, err error) {
+    cur, ok := currencyList[countryName]
+    if !ok {
+	err = fmt.Errorf("country %s not found", countryName)
+	return
+    }
+    curr = *cur
+    return
+}
